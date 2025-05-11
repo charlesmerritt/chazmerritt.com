8a4a1a07080cff0f758f209e59b6415a8d9fbd83
charlesmerritt <cem96047@uga.edu>
2025-05-11 14:56:28 -0400

    new first commit

---
+++ src/app/resources/content.js
@@ -0,0 +1,280 @@
+import { InlineCode } from "@/once-ui/components";
+
+const person = {
+  firstName: "Chaz",
+  lastName: "Merritt",
+  get name() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  role: "ML Researcher",
+  avatar: "/images/avatar.jpg",
+  location: "America/New_York", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Spanish"], // optional: Leave the array empty if you don't want to display languages
+};
+
+const newsletter = {
+  display: true,
+  title: <>Subscribe to {person.firstName}'s Newsletter</>,
+  description: (
+    <>
+      I occasionally share my thoughts on AI, entrepreneurship, and leadership.
+    </>
+  ),
+};
+
+const social = [
+  // Links are automatically displayed.
+  // Import new icons in /once-ui/icons.ts
+  {
+    name: "GitHub",
+    icon: "github",
+    link: "https://github.com/once-ui-system/nextjs-starter",
+  },
+  {
+    name: "LinkedIn",
+    icon: "linkedin",
+    link: "https://www.linkedin.com/company/once-ui/",
+  },
+  {
+    name: "Email",
+    icon: "email",
+    link: "mailto:charles.merritt@uga.edu",
+  },
+];
+
+const home = {
+  label: "Home",
+  title: `${person.name}'s Portfolio`,
+  description: `Portfolio website showcasing my work as a ${person.role}`,
+  headline: <>AI/ML Researcher</>,
+  subline: (
+    <>
+      I'm Chaz, a researcher at the <InlineCode>University of Georgia</InlineCode> where I develop
+      solutions in the forestry space. After hours, I build my own projects.
+    </>
+  ),
+};
+
+const about = {
+  label: "About",
+  title: "About me",
+  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+  tableOfContent: {
+    display: true,
+    subItems: false,
+  },
+  avatar: {
+    display: true,
+  },
+  calendar: {
+    display: true,
+    link: "https://cal.com",
+  },
+  intro: {
+    display: true,
+    title: "Introduction",
+    description: (
+      <>
+	I'm a lifelong learner, AI/ML researcher, and builder. My work bridges theory and application—
+	designing intelligent systems that solve real problems at scale. I’m driven by curiosity, 
+	committed to rigor, and always building toward what's next. Whether it's founding projects or 
+	pushing the limits of machine learning, I stay hands-on, focused, and future-oriented.
+      </>
+    ),
+  },
+  work: {
+    display: true, // set to false to hide this section
+    title: "Work Experience",
+    experiences: [
+      {
+        company: "University of Georgia",
+        timeframe: "2025 - Present",
+        role: "Research Professional",
+        achievements: [
+          <>
+	    Presented at multiple conferences.
+          </>,
+          <>
+	    Published several papers.
+          </>,
+        ],
+        images: [
+          // optional: leave the array empty if you don't want to display images
+          {
+            src: "/images/projects/project-01/cover-01.jpg",
+            alt: "Once UI Project",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+      {
+        company: "Northwest Management",
+        timeframe: "Summer 2024",
+        role: "Geospatial Analyst Internship",
+        achievements: [
+          <>
+            Prototyped automation methods for forest stand delineation. 
+          </>,
+          <>
+	    Utilized self-supervised learning techniques to generate "ground-truth" from unstructured image data. 
+          </>,
+        ],
+        images: [],
+      },
+    ],
+  },
+  studies: {
+    display: true, // set to false to hide this section
+    title: "Studies",
+    institutions: [
+      {
+        name: "University of Georgia, MS in Artificial Intelligence",
+        description: <>Completed my thesis titled 'Harnessing Reinforcement Learning 
+	for Forest Growth Modeling Under Uncertainty' under the supervision of 
+	Dr. Pete Bettinger, Dr. Frederick Maier, and Dr. Stephen Kinane.</>,
+      },
+      {
+        name: "University of Georgia, BA in Cognitive Science, minor in Computer Science",
+        description: <>Studied a broad curriculum where I completed my foundation areas
+	in Artificial Intelligence and Psychology. This was also the time when I first
+	got involved with research in the forestry space on the PERSEUS project.</>,
+      },
+    ],
+  },
+  technical: {
+    display: true, // set to false to hide this section
+    title: "Technical skills",
+    skills: [
+      {
+        title: "Figma",
+        description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [
+          {
+            src: "/images/projects/project-01/cover-02.jpg",
+            alt: "Project image",
+            width: 16,
+            height: 9,
+          },
+          {
+            src: "/images/projects/project-01/cover-03.jpg",
+            alt: "Project image",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+      {
+        title: "Next.js",
+        description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [
+          {
+            src: "/images/projects/project-01/cover-04.jpg",
+            alt: "Project image",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const blog = {
+  label: "Blog",
+  title: "Writing about tech, academia, and my entrepreneurial endeavors...",
+  description: `Read what ${person.name} has been up to recently`,
+  // Create new blog posts by adding a new .mdx file to app/blog/posts
+  // All posts will be listed on the /blog route
+};
+
+const work = {
+  label: "Work",
+  title: "My projects",
+  description: `Design and dev projects by ${person.name}`,
+  // Create new project pages by adding a new .mdx file to app/blog/posts
+  // All projects will be listed on the /home and /work routes
+};
+
+const gallery = {
+  label: "Gallery",
+  title: "My photo gallery",
+  description: `A photo collection by ${person.name}`,
+  // Images from https://pexels.com
+  images: [
+    {
+      src: "/images/gallery/img-01.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-02.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-03.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-04.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-05.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-06.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-07.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-08.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-09.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-10.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-11.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-12.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-13.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-14.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+  ],
+};
+
+export { person, social, newsletter, home, about, blog, work, gallery };
